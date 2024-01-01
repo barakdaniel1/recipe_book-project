ccd4dfa5b64449ed4f0b300ff9b3ef3e30a4399c
barakdaniel1 <barakdaniel1997@gmail.com>
2024-01-01 12:40:26 +0200

    changed allowed origins

--- Server/config/allowedOrigins.js
+++ Server/config/allowedOrigins.js
@@ -1,7 +1,5 @@
 const allowedOrigins = [
-    'https://mysite.com',
-    'http://localhost:5000',
-    'http://127.0.0.1:5500',
+    'https://recipe_book.onredner.com',
     'http://localhost:3000'
 ]
 
