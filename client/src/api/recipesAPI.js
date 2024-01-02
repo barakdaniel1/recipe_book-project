a36885cb73f898f007007809dd83cd68b8ad2896
barakdaniel1 <barakdaniel1997@gmail.com>
2024-01-02 15:41:26 +0200

    Adding content-type header to http requests

--- client/src/api/recipesAPI.js
+++ client/src/api/recipesAPI.js
@@ -7,7 +7,7 @@ const URL = process.env.NODE_ENV === 'production' ? "https://recipe-book-api-3nm
 export const getRecipes = async (userName,accessToken) => {
     try{
         const res = await axios.get(`${URL}/users/${userName}/recipes`,
-        {headers: {Authorization: `Bearer ${accessToken}`},withCredentials: true});
+        {headers: {Authorization: `Bearer ${accessToken}`, 'Content-Type': 'application/json'},withCredentials: true});
         return res.data;
     }
     catch (err) {
@@ -18,7 +18,7 @@ export const getRecipes = async (userName,accessToken) => {
 export const addRecipe = async (userName, accessToken, recipe) => {
     try{
     const res = await axios.post(`${URL}/users/${userName}/recipes`,recipe
-                        ,{headers:{Authorization: `Bearer ${accessToken}`}, withCredentials: true});
+                        ,{headers:{Authorization: `Bearer ${accessToken}`, 'Content-Type': 'application/json'}, withCredentials: true});
         return res;
     } catch (err){
         throw new Error(err.response.data.message);
@@ -29,7 +29,7 @@ export const addRecipe = async (userName, accessToken, recipe) => {
 export const getRecipe = async (userName, accessToken, recipename) => {
     try {
         const res = await axios.get(`${URL}/users/${userName}/recipes/${recipename}`,
-                                        {headers:{Authorization: `Bearer ${accessToken}`},withCredentials: true});
+                                        {headers:{Authorization: `Bearer ${accessToken}`, 'Content-Type': 'application/json'},withCredentials: true});
         return res.data;
     }
     catch (err){
@@ -41,7 +41,7 @@ export const editRecipe = async (userName, accessToken, editedRecipe) => {
     try{
         const res = await axios.put(`${URL}/users/${userName}/recipes`,
                                         editedRecipe,
-                                         {headers: {Authorization: `Bearer ${accessToken}`}, withCredentials: true});
+                                         {headers: {Authorization: `Bearer ${accessToken}`, 'Content-Type': 'application/json'}, withCredentials: true});
         return res.data;        
     } catch (err) {
         throw new Error(err.response.data.message);
@@ -52,7 +52,7 @@ export const deleteRecipe = async (userName,accessToken,recipe_to_delete) => {
  
     try{
        const res = await axios.delete(`${URL}/users/${userName}/recipes`,
-       {headers: {Authorization: `Bearer ${accessToken}`},withCredentials: true,data: {recipename:recipe_to_delete.recipename}});
+       {headers: {Authorization: `Bearer ${accessToken}`, 'Content-Type': 'application/json'},withCredentials: true, data: {recipename:recipe_to_delete.recipename}});
         return res.data;
     }
     catch (err) {
--- client/src/api/usersAPI.js
+++ client/src/api/usersAPI.js
@@ -6,7 +6,7 @@ const URL = process.env.NODE_ENV === 'production' ? "https://recipe-book-api-3nm
 export const registerUser = async (userName, password, email) => {
 
     try {
-        const res = await axios.post(`${URL}/register`,{username:userName, password: password, email: email}, {withCredentials: true});
+        const res = await axios.post(`${URL}/register`,{username:userName, password: password, email: email}, {headers: {'Content-Type': 'application/json'},withCredentials: true});
     }
     catch (err) {
       return err;
@@ -25,7 +25,7 @@ export const logoutUser = async () => {
 
 export const loginUser = async (userName, password) => {
     try {
-        const res = await axios.post(`${URL}/login`, {username: userName, password: password}, {withCredentials: true});
+        const res = await axios.post(`${URL}/login`, {username: userName, password: password}, {headers: {'Content-Type': 'application/json'},withCredentials: true});
         return res;
     }
     catch (err) {
@@ -37,7 +37,7 @@ export const updateUserInfo = async(userName, accessToken, newPassword, newEmail
     try {
         const res = await axios.put(`${URL}/users`,
                     {username: userName, password: newPassword, email: newEmail}, 
-                    {headers: {Authorization: `Bearer ${accessToken}`}, withCredentials: true},
+                    {headers: {Authorization: `Bearer ${accessToken}`, 'Content-Type': 'application/json'}, withCredentials: true},
                     );
         return res;
     }
@@ -50,7 +50,7 @@ export const resetPass = async (email) => {
     try {
         const res = await axios.post(`${URL}/resetPass`, {
             email: email
-        }, {withCredentials: true});
+        }, {headers: {'Content-Type': 'application/json'},withCredentials: true});
         return res;
     }
     catch (err) {
