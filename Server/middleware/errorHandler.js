f782f23004000068f7718ef63a092e70d360bd2d
barakdaniel1 <barakdaniel1997@gmail.com>
2024-01-02 15:52:55 +0200

    added CORS to error handler

--- Server/middleware/errorHandler.js
+++ Server/middleware/errorHandler.js
@@ -1,5 +1,14 @@
+const allowedOrigins = require('../config/allowedOrigins');
+
 const errHandler = (err,req,res,next) => {
     console.error(err.stack);
+    const origin = req.headers.origin;
+    if(allowedOrigins.includes(origin)){
+        res.header('Access-Control-Allow-Origin', origin);
+    }
+
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.status(500).send(err.message);
 }
 
