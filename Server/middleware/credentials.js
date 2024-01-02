c528dc0d220977e9c81937e16375364a8b294d20
barakdaniel1 <barakdaniel1997@gmail.com>
2024-01-02 15:27:46 +0200

    Changing credentials on the backend

--- Server/middleware/credentials.js
+++ Server/middleware/credentials.js
@@ -3,7 +3,17 @@ const allowedOrigins = require('../config/allowedOrigins');
 const credentials = (req,res,next) => {
     const origin = req.headers.origin;
     if(allowedOrigins.includes(origin)){
+        res.header('Access-Control-Allow-Origin', origin);
         res.header('Access-Control-Allow-Credentials',true); 
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+
+        if(req.method === 'OPTIONS'){
+            res.sendStatus(200);
+        }
+        else{
+            next();
+        }
     }
     next();
 }
